daa8d339516e3444dd5a57925da796b2285cf2b3
julia servetnik <juliaservetnik2019@gmail.com>
2020-04-22 14:10:34 +0300

    Init

---
+++ src/test/java/com/pizzaChili/FirstPizza.java
@@ -0,0 +1,19 @@
+package com.pizzaChili;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstPizza {
+
+    @Test
+    public void FirstPizza() {
+        System.setProperty("webdriver.chrome.driver", "C:/Java/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.pizzatempo.by/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Заказ и доставка пиццы на дом в Минске - «Пицца Темпо»"));
+        //driver.quit(); //to close
+    }
+}
