25acee572b80a2a8394fd131503a180232070be6
julia servetnik <juliaservetnik2019@gmail.com>
2020-04-30 16:01:29 +0300

    before and after

--- src/test/java/com/pizzaChili/FirstPizza.java
+++ src/test/java/com/pizzaChili/FirstPizza.java
@@ -1,19 +1,34 @@
 package com.pizzaChili;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstPizza {
 
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:/Java/chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("show start");
+    }
+
     @Test
     public void FirstPizza() {
-        System.setProperty("webdriver.chrome.driver", "C:/Java/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
 
         driver.get("https://www.pizzatempo.by/");
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Заказ и доставка пиццы на дом в Минске - «Пицца Темпо»"));
-        //driver.quit(); //to close
     }
+
+    @After
+    public void stopIt() {
+        driver.quit();
+        System.out.println("show stop");
+    }
+
 }
