e9caef09df6780fa162a42c59373f3f789269f58
julia servetnik <juliaservetnik2019@gmail.com>
2020-05-04 15:58:43 +0300

    run pages

--- src/test/java/com/pizzaChili/FirstPizza.java
+++ src/test/java/com/pizzaChili/FirstPizza.java
@@ -1,38 +1,38 @@
 package com.pizzaChili;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 class FirstPizza {
 
     static ChromeDriver driver;
+
     @BeforeAll
     static void startIt() {
         System.setProperty("webdriver.chrome.driver", "C:/Java/chromedriver.exe");
         driver = new ChromeDriver();
-        System.out.println("show start");
+        System.out.println("Test is Started");
     }
 
     @Test
-    @Order(3)
     void firstPizza() {
-//        driver.manage().window().setSize(new Dimension(500,500));
         driver.get("https://www.pizzatempo.by/");
         String title = driver.getTitle();
         Assertions.assertEquals("Заказ и доставка пиццы на дом в Минске - «Пицца Темпо»", title);
-    }
-
-    @Test
-    @Order(1)
-    void firstPizza2() {
         driver.manage().window().maximize();
         driver.get("https://www.pizzatempo.by/");
-    }
+        WebElement el = driver.findElement(By.linkText("Регистрация"));
+        el.click();
+        System.out.println("registration");
+        el = driver.findElement(By.linkText("Забыли пароль?"));
+        el.click();
+        System.out.println("click");
+     }
 
-    @AfterEach
-    void stopIt() {
+    @AfterAll
+    static void stopIt() {
         driver.quit();
-        System.out.println("show stop");
+        System.out.println("Test is Stopped");
     }
-
-
 }
